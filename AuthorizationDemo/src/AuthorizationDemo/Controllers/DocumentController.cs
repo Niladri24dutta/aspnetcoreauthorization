3676eb0c63112851442ac36933c75069f11a7e7f
Niladri28dutta <nild2141@gmail.com>
2017-10-28 17:33:38 +0530

    Add new Authorization handler for resource based authorization

---
+++ AuthorizationDemo/src/AuthorizationDemo/Controllers/DocumentController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+
+namespace AuthorizationLab.Controllers
+{
+    
+    public class DocumentController : Controller
+    {
+        private readonly IDocumentRepository _documentRepository;
+        private readonly IAuthorizationService _authService;
+
+        public DocumentController(IDocumentRepository repository, IAuthorizationService service)
+        {
+            _documentRepository = repository;
+            _authService = service;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_documentRepository.GetDocuments());
+        }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            var document = _documentRepository.GetDocumentById(id);
+            if(document == null)
+            {
+                return new NotFoundResult();
+            }
+            if(await _authService.AuthorizeAsync(User,document,new EditRequirement()))
+            {
+                return View(document);
+            }
+            else
+            {
+                return new ChallengeResult();
+            }
+        }
+    }
+}
---
+++ AuthorizationDemo/src/AuthorizationDemo/DocumentRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AuthorizationLab
+{
+    public class DocumentRepository : IDocumentRepository
+    {
+        static List<Document> _documents = new List<Document>
+        {
+            new Document {Id = 1, Author="Niladri" },
+            new Document { Id = 2,Author = "Amarjit" }
+        };
+        public Document GetDocumentById(int id)
+        {
+            return _documents.FirstOrDefault(x => x.Id == id);
+        }
+
+        public IEnumerable<Document> GetDocuments()
+        {
+            return _documents;
+        }
+    }
+}
